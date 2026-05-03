1487de49ac506f389932915d44c5bc20f7096334
husszzzz <hussien2010rtx@gmail.com>
2026-05-04 02:30:20 +0300

    Refactor SmartButton for secure coding support

--- main.m
+++ main.m
@@ -1,23 +1,26 @@
 #import <UIKit/UIKit.h>
 
 // نموذج البيانات للزر
-@interface SmartButton : NSObject <NSCoding>
+@interface SmartButton : NSObject <NSSecureCoding>
 @property (nonatomic, strong) NSString *name;
-@property (nonatomic, strong) NSString *type; // "link" أو "text"
+@property (nonatomic, strong) NSString *type;
 @property (nonatomic, strong) NSString *content;
 @end
 
 @implementation SmartButton
++ (BOOL)supportsSecureCoding { return YES; }
+
 - (void)encodeWithCoder:(NSCoder *)encoder {
     [encoder encodeObject:self.name forKey:@"name"];
     [encoder encodeObject:self.type forKey:@"type"];
     [encoder encodeObject:self.content forKey:@"content"];
 }
+
 - (id)initWithCoder:(NSCoder *)decoder {
     if (self = [super init]) {
-        self.name = [decoder decodeObjectForKey:@"name"];
-        self.type = [decoder decodeObjectForKey:@"type"];
-        self.content = [decoder decodeObjectForKey:@"content"];
+        self.name = [decoder decodeObjectOfClass:[NSString class] forKey:@"name"];
+        self.type = [decoder decodeObjectOfClass:[NSString class] forKey:@"type"];
+        self.content = [decoder decodeObjectOfClass:[NSString class] forKey:@"content"];
     }
     return self;
 }
@@ -35,17 +38,15 @@
     self.title = @"أزراري الذكية";
     self.view.backgroundColor = [UIColor systemGroupedBackgroundColor];
     
-    // تحميل البيانات المحفوظة
     [self loadButtons];
     
-    // أزرار الشريط العلوي
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(addNewButton)];
     self.navigationItem.leftBarButtonItem = self.editButtonItem;
 }
 
-// --- حفظ وتحميل البيانات باستخدام UserDefaults ---
 - (void)saveButtons {
-    NSData *data = [NSKeyedArchiver archivedDataWithRootObject:self.buttons requiringSecureCoding:NO error:nil];
+    NSError *error;
+    NSData *data = [NSKeyedArchiver archivedDataWithRootObject:self.buttons requiringSecureCoding:NO error:&error];
     [[NSUserDefaults standardUserDefaults] setObject:data forKey:@"HassanyButtons"];
     [[NSUserDefaults standardUserDefaults] synchronize];
 }
@@ -53,83 +54,72 @@
 - (void)loadButtons {
     NSData *data = [[NSUserDefaults standardUserDefaults] objectForKey:@"HassanyButtons"];
     if (data) {
-        self.buttons = [NSKeyedUnarchiver unarchiveObjectWithData:data];
-    } else {
+        NSSet *set = [NSSet setWithArray:@[[NSMutableArray class], [SmartButton class]]];
+        self.buttons = [NSKeyedUnarchiver unarchivedObjectOfClasses:set fromData:data error:nil];
+    }
+    if (!self.buttons) {
         self.buttons = [[NSMutableArray alloc] init];
     }
 }
 
-// --- إضافة زر جديد (Alert مع حقول إدخال) ---
 - (void)addNewButton {
     UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"إضافة زر جديد" message:nil preferredStyle:UIAlertControllerStyleAlert];
+    [alert addTextFieldWithConfigurationHandler:^(UITextField *t) { t.placeholder = @"اسم الزر"; }];
+    [alert addTextFieldWithConfigurationHandler:^(UITextField *t) { t.placeholder = @"الرابط أو النص"; }];
     
-    [alert addTextFieldWithConfigurationHandler:^(UITextField *textField) { textField.placeholder = @"اسم الزر"; }];
-    [alert addTextFieldWithConfigurationHandler:^(UITextField *textField) { textField.placeholder = @"الرابط أو النص"; }];
-    
-    UIAlertAction *save = [UIAlertAction actionWithTitle:@"حفظ" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
-        SmartButton *newBtn = [[SmartButton alloc] init];
-        newBtn.name = alert.textFields[0].text;
-        newBtn.content = alert.textFields[1].text;
-        newBtn.type = [newBtn.content containsString:@"http"] ? @"link" : @"text";
-        
-        [self.buttons addObject:newBtn];
+    UIAlertAction *save = [UIAlertAction actionWithTitle:@"حفظ" style:UIAlertActionStyleDefault handler:^(UIAlertAction *a) {
+        SmartButton *nb = [[SmartButton alloc] init];
+        nb.name = alert.textFields[0].text;
+        nb.content = alert.textFields[1].text;
+        nb.type = [nb.content hasPrefix:@"http"] ? @"link" : @"text";
+        [self.buttons addObject:nb];
         [self saveButtons];
         [self.tableView reloadData];
     }];
-    
     [alert addAction:save];
     [alert addAction:[UIAlertAction actionWithTitle:@"إلغاء" style:UIAlertActionStyleCancel handler:nil]];
     [self presentViewController:alert animated:YES completion:nil];
 }
 
-// --- إعدادات الجدول (List) ---
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.buttons.count;
+- (NSInteger)tableView:(UITableView *)tv numberOfRowsInSection:(NSInteger)s { return self.buttons.count; }
+
+- (UITableViewCell *)tableView:(UITableView *)tv cellForRowAtIndexPath:(NSIndexPath *)ip {
+    UITableViewCell *c = [tv dequeueReusableCellWithIdentifier:@"c"] ?: [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"c"];
+    SmartButton *b = self.buttons[ip.row];
+    c.textLabel.text = b.name;
+    c.detailTextLabel.text = b.content;
+    c.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    c.imageView.image = [UIImage systemImageNamed:[b.type isEqualToString:@"link"] ? @"link.circle.fill" : @"text.bubble.fill"];
+    return c;
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"cell"];
-    SmartButton *btn = self.buttons[indexPath.row];
-    
-    cell.textLabel.text = btn.name;
-    cell.detailTextLabel.text = btn.content;
-    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-    cell.imageView.image = [UIImage systemImageNamed:[btn.type isEqualToString:@"link"] ? @"link.circle.fill" : @"text.bubble.fill"];
-    cell.imageView.tintColor = [btn.type isEqualToString:@"link"] ? [UIColor systemBlueColor] : [UIColor systemGreenColor];
-    
-    return cell;
-}
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    SmartButton *btn = self.buttons[indexPath.row];
-    if ([btn.type isEqualToString:@"link"]) {
-        [[UIApplication sharedApplication] openURL:https://developer.apple.com/documentation/foundation/nsurl/urlwithstring:?language=objc options:@{} completionHandler:nil];
+- (void)tableView:(UITableView *)tv didSelectRowAtIndexPath:(NSIndexPath *)ip {
+    SmartButton *b = self.buttons[ip.row];
+    if ([b.type isEqualToString:@"link"]) {
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:b.content] options:@{} completionHandler:nil];
     } else {
-        UIAlertController *note = [UIAlertController alertControllerWithTitle:btn.name message:btn.content preferredStyle:UIAlertControllerStyleAlert];
-        [note addAction:[UIAlertAction actionWithTitle:@"إغلاق" style:UIAlertActionStyleCancel handler:nil]];
-        [self presentViewController:note animated:YES completion:nil];
+        UIAlertController *n = [UIAlertController alertControllerWithTitle:b.name message:b.content preferredStyle:UIAlertControllerStyleAlert];
+        [n addAction:[UIAlertAction actionWithTitle:@"إغلاق" style:UIAlertActionStyleCancel handler:nil]];
+        [self presentViewController:n animated:YES completion:nil];
     }
 }
 
-// حذف وترتيب
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        [self.buttons removeObjectAtIndex:indexPath.row];
+- (void)tableView:(UITableView *)tv commitEditingStyle:(UITableViewCellEditingStyle)s forRowAtIndexPath:(NSIndexPath *)ip {
+    if (s == UITableViewCellEditingStyleDelete) {
+        [self.buttons removeObjectAtIndex:ip.row];
         [self saveButtons];
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+        [tv deleteRowsAtIndexPaths:@[ip] withRowAnimation:UITableViewRowAnimationFade];
     }
 }
 @end
 
-// --- تشغيل التطبيق ---
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 @property (strong, nonatomic) UIWindow *window;
 @end
 @implementation AppDelegate
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+- (BOOL)application:(UIApplication *)app didFinishLaunchingWithOptions:(NSDictionary *)opt {
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:[[RootViewController alloc] init]];
-    self.window.rootViewController = nav;
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[RootViewController alloc] init]];
     [self.window makeKeyAndVisible];
     return YES;
 }
