ff2c90bb2a4f44962494bbd09d34672e068f51ee
husszzzz <hussien2010rtx@gmail.com>
2026-05-04 02:41:44 +0300

    Update main.m

--- main.m
+++ main.m
@@ -1,125 +1,88 @@
 #import <UIKit/UIKit.h>
+#import <WebKit/WebKit.h>
 
-// نموذج البيانات للزر
-@interface SmartButton : NSObject <NSSecureCoding>
-@property (nonatomic, strong) NSString *name;
-@property (nonatomic, strong) NSString *type;
-@property (nonatomic, strong) NSString *content;
-@end
-
-@implementation SmartButton
-+ (BOOL)supportsSecureCoding { return YES; }
-
-- (void)encodeWithCoder:(NSCoder *)encoder {
-    [encoder encodeObject:self.name forKey:@"name"];
-    [encoder encodeObject:self.type forKey:@"type"];
-    [encoder encodeObject:self.content forKey:@"content"];
-}
-
-- (id)initWithCoder:(NSCoder *)decoder {
-    if (self = [super init]) {
-        self.name = [decoder decodeObjectOfClass:[NSString class] forKey:@"name"];
-        self.type = [decoder decodeObjectOfClass:[NSString class] forKey:@"type"];
-        self.content = [decoder decodeObjectOfClass:[NSString class] forKey:@"content"];
-    }
-    return self;
-}
-@end
-
-// الواجهة الرئيسية
-@interface RootViewController : UITableViewController
-@property (nonatomic, strong) NSMutableArray<SmartButton *> *buttons;
+@interface RootViewController : UIViewController <WKNavigationDelegate, WKUIDelegate>
+@property (nonatomic, strong) WKWebView *webView;
 @end
 
 @implementation RootViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.title = @"أزراري الذكية";
-    self.view.backgroundColor = [UIColor systemGroupedBackgroundColor];
     
-    [self loadButtons];
+    // خلفية سوداء حتى ما يبين أي بياض أثناء التحميل
+    self.view.backgroundColor = [UIColor blackColor];
+
+    // 1. إعطاء الصلاحيات الكاملة للموقع (جافا سكربت، وتخزين محلي)
+    WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
+    WKPreferences *prefs = [[WKPreferences alloc] init];
+    prefs.javaScriptCanOpenWindowsAutomatically = YES;
+    config.preferences = prefs;
+    config.websiteDataStore = [WKWebsiteDataStore defaultDataStore];
+
+    // 2. بناء الواجهة
+    self.webView = [[WKWebView alloc] initWithFrame:self.view.bounds configuration:config];
+    self.webView.navigationDelegate = self;
+    self.webView.UIDelegate = self; // ضروري جداً لتشغيل أزرار موقعك بشكل صحيح
     
-    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(addNewButton)];
-    self.navigationItem.leftBarButtonItem = self.editButtonItem;
+    // إعدادات تجربة المستخدم
+    self.webView.allowsBackForwardNavigationGestures = YES;
+    self.webView.scrollView.bounces = NO; // منع السحب المطاطي المزعج
+    self.webView.scrollView.showsVerticalScrollIndicator = NO;
+    self.webView.scrollView.showsHorizontalScrollIndicator = NO;
+    
+    // جعل الموقع يملأ الشاشة بالكامل (تجاهل النوتش)
+    self.webView.scrollView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
+
+    [self.view addSubview:self.webView];
+
+    // 3. مسح الذاكرة المؤقتة لضمان التحديث التلقائي
+    [[NSURLCache sharedURLCache] removeAllCachedResponses];
+    
+    // 4. تحميل الرابط مالتك (مع إجبار جلب أحدث نسخة دائماً)
+    NSURL *url = [NSURL URLWithString:@"https://husszzzz.github.io/Moon/"];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url 
+                                                           cachePolicy:NSURLRequestReloadIgnoringLocalCacheData 
+                                                       timeoutInterval:30.0];
+    
+    [self.webView loadRequest:request];
 }
 
-- (void)saveButtons {
-    NSError *error;
-    NSData *data = [NSKeyedArchiver archivedDataWithRootObject:self.buttons requiringSecureCoding:NO error:&error];
-    [[NSUserDefaults standardUserDefaults] setObject:data forKey:@"HassanyButtons"];
-    [[NSUserDefaults standardUserDefaults] synchronize];
+- (void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    self.webView.frame = self.view.bounds;
 }
 
-- (void)loadButtons {
-    NSData *data = [[NSUserDefaults standardUserDefaults] objectForKey:@"HassanyButtons"];
-    if (data) {
-        NSSet *set = [NSSet setWithArray:@[[NSMutableArray class], [SmartButton class]]];
-        self.buttons = [NSKeyedUnarchiver unarchivedObjectOfClasses:set fromData:data error:nil];
-    }
-    if (!self.buttons) {
-        self.buttons = [[NSMutableArray alloc] init];
-    }
-}
+// --- 5. ربط أوامر جافا سكربت بنظام الآيفون ---
 
-- (void)addNewButton {
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"إضافة زر جديد" message:nil preferredStyle:UIAlertControllerStyleAlert];
-    [alert addTextFieldWithConfigurationHandler:^(UITextField *t) { t.placeholder = @"اسم الزر"; }];
-    [alert addTextFieldWithConfigurationHandler:^(UITextField *t) { t.placeholder = @"الرابط أو النص"; }];
-    
-    UIAlertAction *save = [UIAlertAction actionWithTitle:@"حفظ" style:UIAlertActionStyleDefault handler:^(UIAlertAction *a) {
-        SmartButton *nb = [[SmartButton alloc] init];
-        nb.name = alert.textFields[0].text;
-        nb.content = alert.textFields[1].text;
-        nb.type = [nb.content hasPrefix:@"http"] ? @"link" : @"text";
-        [self.buttons addObject:nb];
-        [self saveButtons];
-        [self.tableView reloadData];
-    }];
-    [alert addAction:save];
-    [alert addAction:[UIAlertAction actionWithTitle:@"إلغاء" style:UIAlertActionStyleCancel handler:nil]];
+// إذا موقعك طلب Alert
+- (void)webView:(WKWebView *)webView runJavaScriptAlertPanelWithMessage:(NSString *)message initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(void))completionHandler {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Moon" message:message preferredStyle:UIAlertControllerStyleAlert];
+    [alert addAction:[UIAlertAction actionWithTitle:@"موافق" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        completionHandler();
+    }]];
     [self presentViewController:alert animated:YES completion:nil];
 }
 
-- (NSInteger)tableView:(UITableView *)tv numberOfRowsInSection:(NSInteger)s { return self.buttons.count; }
-
-- (UITableViewCell *)tableView:(UITableView *)tv cellForRowAtIndexPath:(NSIndexPath *)ip {
-    UITableViewCell *c = [tv dequeueReusableCellWithIdentifier:@"c"] ?: [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"c"];
-    SmartButton *b = self.buttons[ip.row];
-    c.textLabel.text = b.name;
-    c.detailTextLabel.text = b.content;
-    c.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-    c.imageView.image = [UIImage systemImageNamed:[b.type isEqualToString:@"link"] ? @"link.circle.fill" : @"text.bubble.fill"];
-    return c;
+// إذا موقعك طلب Confirm (موافق/إلغاء)
+- (void)webView:(WKWebView *)webView runJavaScriptConfirmPanelWithMessage:(NSString *)message initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(BOOL result))completionHandler {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Moon" message:message preferredStyle:UIAlertControllerStyleAlert];
+    [alert addAction:[UIAlertAction actionWithTitle:@"موافق" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) { completionHandler(YES); }]];
+    [alert addAction:[UIAlertAction actionWithTitle:@"إلغاء" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) { completionHandler(NO); }]];
+    [self presentViewController:alert animated:YES completion:nil];
 }
 
-- (void)tableView:(UITableView *)tv didSelectRowAtIndexPath:(NSIndexPath *)ip {
-    SmartButton *b = self.buttons[ip.row];
-    if ([b.type isEqualToString:@"link"]) {
-        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:b.content] options:@{} completionHandler:nil];
-    } else {
-        UIAlertController *n = [UIAlertController alertControllerWithTitle:b.name message:b.content preferredStyle:UIAlertControllerStyleAlert];
-        [n addAction:[UIAlertAction actionWithTitle:@"إغلاق" style:UIAlertActionStyleCancel handler:nil]];
-        [self presentViewController:n animated:YES completion:nil];
-    }
-}
-
-- (void)tableView:(UITableView *)tv commitEditingStyle:(UITableViewCellEditingStyle)s forRowAtIndexPath:(NSIndexPath *)ip {
-    if (s == UITableViewCellEditingStyleDelete) {
-        [self.buttons removeObjectAtIndex:ip.row];
-        [self saveButtons];
-        [tv deleteRowsAtIndexPaths:@[ip] withRowAnimation:UITableViewRowAnimationFade];
-    }
-}
 @end
 
+// --- إدارة تشغيل التطبيق ---
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 @property (strong, nonatomic) UIWindow *window;
 @end
+
 @implementation AppDelegate
 - (BOOL)application:(UIApplication *)app didFinishLaunchingWithOptions:(NSDictionary *)opt {
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[RootViewController alloc] init]];
+    self.window.rootViewController = [[RootViewController alloc] init];
     [self.window makeKeyAndVisible];
     return YES;
 }
