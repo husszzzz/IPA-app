3707264c9167b4efa5bc6175d0f51af43b0414b4
husszzzz <hussien2010rtx@gmail.com>
2026-05-04 02:20:04 +0300

    Refactor UI elements in RootViewController
    Updated the background color and button design in RootViewController.

--- main.m
+++ main.m
@@ -1,33 +1,60 @@
 #import <UIKit/UIKit.h>
 
+// --- واجهة التحكم الرئيسية ---
 @interface RootViewController : UIViewController
 @end
 
 @implementation RootViewController
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor groupTableViewBackgroundColor];
+    
+    // خلفية بيضاء نظيفة تدعم الوضع الليلي تلقائياً
+    self.view.backgroundColor = [UIColor systemBackgroundColor];
     self.title = @"Moon Manager";
 
-    // زر الأول: إدارة الملفات
-    UIButton *btn1 = [self createButtonWithTitle:@"إدارة الملفات" yPostion:200 color:[UIColor systemBlueColor]];
-    [self.view addSubview:btn1];
+    // تصميم زر احترافي
+    UIButton *actionButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    actionButton.frame = CGRectMake(40, 250, self.view.frame.size.width - 80, 60);
+    [actionButton setTitle:@"إدارة ملفات Moon" forState:UIControlStateNormal];
+    
+    // ستايل الزر
+    actionButton.backgroundColor = [UIColor systemBlueColor];
+    [actionButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    actionButton.layer.cornerRadius = 15;
+    actionButton.titleLabel.font = [UIFont boldSystemFontOfSize:18];
+    
+    // إضافة ظل خفيف للجمالية
+    actionButton.layer.shadowColor = [UIColor blackColor].CGColor;
+    actionButton.layer.shadowOffset = CGSizeMake(0, 4);
+    actionButton.layer.shadowOpacity = 0.2;
+    actionButton.layer.shadowRadius = 5;
 
-    // زر الثاني: الإعدادات
-    UIButton *btn2 = [self createButtonWithTitle:@"الإعدادات" yPostion:270 color:[UIColor systemIndigoColor]];
-    [self.view addSubview:btn2];
-}
-
-- (UIButton *)createButtonWithTitle:(NSString *)title yPostion:(CGFloat)y color:(UIColor *)color {
-    UIButton *btn = [UIButton buttonWithType:UIButtonTypeSystem];
-    btn.frame = CGRectMake(50, y, self.view.frame.size.width - 100, 50);
-    [btn setTitle:title forState:UIControlStateNormal];
-    btn.backgroundColor = color;
-    [btn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    btn.layer.cornerRadius = 12;
-    btn.titleLabel.font = [UIFont boldSystemFontOfSize:16];
-    return btn;
+    [self.view addSubview:actionButton];
 }
 @end
 
-// بقية الكود الأساسي (AppDelegate و Main) يفضل بقاؤه كما هو لضمان التشغيل
+// --- إدارة تشغيل التطبيق ---
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@property (strong, nonatomic) UIWindow *window;
+@end
+
+@implementation AppDelegate
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    
+    // وضع التطبيق داخل Navigation Controller لإضافة شريط علوي
+    RootViewController *rootVC = [[RootViewController alloc] init];
+    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:rootVC];
+    
+    self.window.rootViewController = nav;
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+@end
+
+// --- نقطة الانطلاق ---
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
