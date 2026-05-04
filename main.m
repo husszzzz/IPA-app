4cdd73eb778563402d03b920bb91c61561545465
husszzzz <hussien2010rtx@gmail.com>
2026-05-04 12:07:21 +0300

    Refactor audio control UI and functionality

--- main.m
+++ main.m
@@ -3,10 +3,10 @@
 
 @interface RootViewController : UIViewController
 @property (nonatomic, strong) AVAudioEngine *audioEngine;
-@property (nonatomic, strong) AVAudioMixerNode *boosterNode;
-@property (nonatomic, strong) UISlider *gainSlider;
-@property (nonatomic, strong) UILabel *gainLabel;
-@property (nonatomic, strong) UIButton *actionButton;
+@property (nonatomic, strong) AVAudioMixerNode *mainBooster;
+@property (nonatomic, strong) UISlider *powerSlider;
+@property (nonatomic, strong) UILabel *statusLabel;
+@property (nonatomic, strong) UIButton *toggleButton;
 @end
 
 @implementation RootViewController
@@ -14,62 +14,58 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor blackColor];
-    
-    // واجهة Hassany الاحترافية
-    [self setupProfessionalUI];
+    [self setupPureUI];
     
     self.audioEngine = [[AVAudioEngine alloc] init];
-    self.boosterNode = [[AVAudioMixerNode alloc] init];
+    self.mainBooster = [[AVAudioMixerNode alloc] init];
 }
 
-- (void)setupProfessionalUI {
-    CGFloat w = self.view.frame.size.width;
+- (void)setupPureUI {
+    CGFloat screenW = self.view.frame.size.width;
     
-    UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(0, 70, w, 50)];
-    title.text = @"HASSANY PURE BOOST";
-    title.textColor = [UIColor systemYellowColor];
-    title.font = [UIFont boldSystemFontOfSize:28];
-    title.textAlignment = NSTextAlignmentCenter;
-    [self.view addSubview:title];
+    UILabel *header = [[UILabel alloc] initWithFrame:CGRectMake(0, 80, screenW, 50)];
+    header.text = @"HASSANY PURE SOUND";
+    header.textColor = [UIColor whiteColor];
+    header.font = [UIFont boldSystemFontOfSize:26];
+    header.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:header];
 
-    // زر التشغيل
-    self.actionButton = [UIButton buttonWithType:UIButtonTypeCustom];
-    self.actionButton.frame = CGRectMake((w-140)/2, 180, 140, 140);
-    self.actionButton.layer.cornerRadius = 70;
-    self.actionButton.layer.borderWidth = 5;
-    self.actionButton.layer.borderColor = [UIColor systemBlueColor].CGColor;
-    [self.actionButton setTitle:@"START" forState:UIControlStateNormal];
-    [self.actionButton setTitleColor:[UIColor systemBlueColor] forState:UIControlStateNormal];
-    [self.actionButton addTarget:self action:@selector(handleAction) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:self.actionButton];
+    // زر التشغيل - تم استبدال الألوان المسببة للمشاكل بألوان نظام أساسية
+    self.toggleButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    self.toggleButton.frame = CGRectMake((screenW-150)/2, 180, 150, 150);
+    self.toggleButton.layer.cornerRadius = 75;
+    self.toggleButton.layer.borderWidth = 4;
+    self.toggleButton.layer.borderColor = [UIColor blueColor].CGColor;
+    [self.toggleButton setTitle:@"تشغيل" forState:UIControlStateNormal];
+    [self.toggleButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+    [self.toggleButton addTarget:self action:@selector(togglePower) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:self.toggleButton];
 
     // سلايدر القوة
-    self.gainSlider = [[UISlider alloc] initWithFrame:CGRectMake(40, 380, w-80, 40)];
-    self.gainSlider.minimumValue = 1.0;
-    self.gainSlider.maximumValue = 40.0; // 40 ضعف كافية جداً للنقاء
-    self.gainSlider.value = 5.0;
-    [self.gainSlider addTarget:self action:@selector(volumeChanged) forControlEvents:UIControlEventValueChanged];
-    [self.view addSubview:self.gainSlider];
+    self.powerSlider = [[UISlider alloc] initWithFrame:CGRectMake(40, 400, screenW-80, 40)];
+    self.powerSlider.minimumValue = 1.0;
+    self.powerSlider.maximumValue = 35.0; // قوة 35 ضعف تعتبر الحد الأقصى للنقاء
+    self.powerSlider.value = 5.0;
+    [self.powerSlider addTarget:self action:@selector(updateVolume) forControlEvents:UIControlEventValueChanged];
+    [self.view addSubview:self.powerSlider];
 
-    self.gainLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 430, w, 30)];
-    self.gainLabel.text = @"قوة التضخيم: 5x";
-    self.gainLabel.textColor = [UIColor whiteColor];
-    self.gainLabel.textAlignment = NSTextAlignmentCenter;
-    [self.view addSubview:self.gainLabel];
+    self.statusLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 450, screenW, 30)];
+    self.statusLabel.text = @"مستوى القوة: 5x";
+    self.statusLabel.textColor = [UIColor grayColor];
+    self.statusLabel.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:self.statusLabel];
 }
 
-- (void)volumeChanged {
-    self.gainLabel.text = [NSString stringWithFormat:@"قوة التضخيم: %dx", (int)self.gainSlider.value];
+- (void)updateVolume {
+    self.statusLabel.text = [NSString stringWithFormat:@"مستوى القوة: %dx", (int)self.powerSlider.value];
     if (self.audioEngine.isRunning) {
-        self.boosterNode.outputVolume = self.gainSlider.value;
+        self.mainBooster.outputVolume = self.powerSlider.value;
     }
 }
 
-- (void)handleAction {
+- (void)togglePower {
     if (self.audioEngine.isRunning) {
-        [self.audioEngine stop];
-        [self.actionButton setTitle:@"START" forState:UIControlStateNormal];
-        self.actionButton.backgroundColor = [UIColor clearColor];
+        [self stopEngine];
     } else {
         [self startPureEngine];
     }
@@ -77,32 +73,38 @@
 
 - (void)startPureEngine {
     AVAudioSession *session = [AVAudioSession sharedInstance];
-    // سر النقاء: استخدام ModeMeasurement أو VoiceChat لتقليل المعالجة الخارجية والضجيج
+    // ضبط الجلسة لوضع المحادثة الصوتية لتفعيل ميزات عزل الصدى والضجيج برمجياً
     [session setCategory:AVAudioSessionCategoryPlayAndRecord 
              withOptions:AVAudioSessionCategoryOptionAllowBluetooth | AVAudioSessionCategoryOptionDefaultToSpeaker 
                    error:nil];
     [session setMode:AVAudioSessionModeVoiceChat error:nil];
     [session setActive:YES error:nil];
 
-    [self.audioEngine attachNode:self.boosterNode];
+    [self.audioEngine attachNode:self.mainBooster];
     
     AVAudioInputNode *input = self.audioEngine.inputNode;
     AVAudioFormat *format = [input inputFormatForBus:0];
 
-    // توصيل المايك بالمضخم ثم للمخرج
-    [self.audioEngine connect:input to:self.boosterNode format:format];
-    [self.audioEngine connect:self.boosterNode to:self.audioEngine.mainMixerNode format:format];
+    [self.audioEngine connect:input to:self.mainBooster format:format];
+    [self.audioEngine connect:self.mainBooster to:self.audioEngine.mainMixerNode format:format];
 
-    self.boosterNode.outputVolume = self.gainSlider.value;
+    self.mainBooster.outputVolume = self.powerSlider.value;
     
     NSError *error;
     [self.audioEngine startAndReturnError:&error];
     
-    if(!error) {
-        [self.actionButton setTitle:@"STOP" forState:UIControlStateNormal];
-        self.actionButton.backgroundColor = [UIColor systemBlueColor];
-        [self.actionButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    if (!error) {
+        [self.toggleButton setTitle:@"إيقاف" forState:UIControlStateNormal];
+        self.toggleButton.backgroundColor = [UIColor blueColor];
+        [self.toggleButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     }
+}
+
+- (void)stopEngine {
+    [self.audioEngine stop];
+    [self.toggleButton setTitle:@"تشغيل" forState:UIControlStateNormal];
+    self.toggleButton.backgroundColor = [UIColor clearColor];
+    [self.toggleButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
 }
 @end
 
