9a25c8397d0108bda86a89897fb44054b3e0ef33
husszzzz <hussien2010rtx@gmail.com>
2026-05-04 12:01:46 +0300

    Refactor UI components and adjust gain settings
    Updated UI elements and labels for clarity and functionality. Adjusted gain slider range and button properties.

--- main.m
+++ main.m
@@ -7,7 +7,6 @@
 @property (nonatomic, strong) UISlider *gainSlider;
 @property (nonatomic, strong) UILabel *gainValueLabel;
 @property (nonatomic, strong) UIButton *powerButton;
-@property (nonatomic, strong) UILabel *statusLabel;
 @end
 
 @implementation RootViewController
@@ -15,7 +14,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor blackColor];
-    
     [self setupHassanyLayout];
     
     self.audioEngine = [[AVAudioEngine alloc] init];
@@ -25,120 +23,82 @@
 - (void)setupHassanyLayout {
     CGFloat screenWidth = self.view.frame.size.width;
 
-    // عنوان HASSANY PRO - استخدمنا اللون الأصفر (Gold البديل)
     UILabel *headerLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 80, screenWidth, 60)];
-    headerLabel.text = @"HASSANY PRO";
+    headerLabel.text = @"HASSANY CLEAR"; // تحديث الاسم ليعبر عن النقاء
     headerLabel.textColor = [UIColor yellowColor];
     headerLabel.font = [UIFont systemFontOfSize:32 weight:UIFontWeightHeavy];
     headerLabel.textAlignment = NSTextAlignmentCenter;
     [self.view addSubview:headerLabel];
 
-    // لوحة التحكم
-    UIView *card = [[UIView alloc] initWithFrame:CGRectMake(20, 180, screenWidth - 40, 450)];
+    UIView *card = [[UIView alloc] initWithFrame:CGRectMake(20, 180, screenWidth - 40, 400)];
     card.backgroundColor = [UIColor colorWithWhite:0.1 alpha:1.0];
     card.layer.cornerRadius = 40;
     [self.view addSubview:card];
 
-    // زر التشغيل - استخدمنا اللون الأزرق الأساسي
     self.powerButton = [UIButton buttonWithType:UIButtonTypeCustom];
-    self.powerButton.frame = CGRectMake((card.frame.size.width - 140)/2, 40, 140, 140);
-    self.powerButton.layer.cornerRadius = 70;
-    self.powerButton.layer.borderWidth = 4;
+    self.powerButton.frame = CGRectMake((card.frame.size.width - 120)/2, 40, 120, 120);
+    self.powerButton.layer.cornerRadius = 60;
+    self.powerButton.layer.borderWidth = 3;
     self.powerButton.layer.borderColor = [UIColor systemBlueColor].CGColor;
-    [self.powerButton setTitle:@"OFF" forState:UIControlStateNormal];
-    [self.powerButton setTitleColor:[UIColor systemBlueColor] forState:UIControlStateNormal];
-    self.powerButton.titleLabel.font = [UIFont systemFontOfSize:28 weight:UIFontWeightBold];
+    [self.powerButton setTitle:@"تشغيل" forState:UIControlStateNormal];
     [self.powerButton addTarget:self action:@selector(togglePower) forControlEvents:UIControlEventTouchUpInside];
     [card addSubview:self.powerButton];
 
-    // سلايدر التضخيم (1x to 100x)
-    self.gainSlider = [[UISlider alloc] initWithFrame:CGRectMake(30, 260, card.frame.size.width - 60, 40)];
+    self.gainSlider = [[UISlider alloc] initWithFrame:CGRectMake(30, 220, card.frame.size.width - 60, 40)];
     self.gainSlider.minimumValue = 1.0;
-    self.gainSlider.maximumValue = 100.0;
-    self.gainSlider.value = 1.0;
-    self.gainSlider.minimumTrackTintColor = [UIColor systemBlueColor];
+    self.gainSlider.maximumValue = 50.0; // قللتها لـ 50 للحفاظ على النقاء، الـ 100 تسبب تشويش عالي
+    self.gainSlider.value = 5.0;
     [self.gainSlider addTarget:self action:@selector(gainChanged:) forControlEvents:UIControlEventValueChanged];
     [card addSubview:self.gainSlider];
 
-    // عرض رقم التضخيم
-    self.gainValueLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 310, card.frame.size.width, 40)];
-    self.gainValueLabel.text = @"BOOST: 1x";
+    self.gainValueLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 270, card.frame.size.width, 40)];
+    self.gainValueLabel.text = @"قوة التصفية: 5x";
     self.gainValueLabel.textColor = [UIColor whiteColor];
-    self.gainValueLabel.font = [UIFont boldSystemFontOfSize:22];
     self.gainValueLabel.textAlignment = NSTextAlignmentCenter;
     [card addSubview:self.gainValueLabel];
-
-    self.statusLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 380, card.frame.size.width, 30)];
-    self.statusLabel.text = @"استخدم السماعة لتجنب الصفير";
-    self.statusLabel.textColor = [UIColor lightGrayColor];
-    self.statusLabel.font = [UIFont systemFontOfSize:14];
-    self.statusLabel.textAlignment = NSTextAlignmentCenter;
-    [card addSubview:self.statusLabel];
-    
-    UILabel *footer = [[UILabel alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height - 60, screenWidth, 30)];
-    footer.text = @"Dev by @OM_G9";
-    footer.textColor = [UIColor darkGrayColor];
-    footer.textAlignment = NSTextAlignmentCenter;
-    [self.view addSubview:footer];
 }
 
 - (void)gainChanged:(UISlider *)sender {
-    int value = (int)sender.value;
-    self.gainValueLabel.text = [NSString stringWithFormat:@"BOOST: %dx", value];
+    self.gainValueLabel.text = [NSString stringWithFormat:@"قوة التصفية: %dx", (int)sender.value];
     if (self.audioEngine.isRunning) {
         self.mixerNode.outputVolume = sender.value;
     }
 }
 
 - (void)togglePower {
-    if (self.audioEngine.isRunning) {
-        [self stopBoosting];
-    } else {
-        [self startBoosting];
-    }
+    if (self.audioEngine.isRunning) { [self stopBoosting]; } else { [self startBoosting]; }
 }
 
 - (void)startBoosting {
     AVAudioSession *session = [AVAudioSession sharedInstance];
+    
+    // الحل السحري: استخدام ModeVoiceChat لعزل الصدى والضجيج تلقائياً
     [session setCategory:AVAudioSessionCategoryPlayAndRecord 
              withOptions:AVAudioSessionCategoryOptionAllowBluetooth | AVAudioSessionCategoryOptionDefaultToSpeaker
                    error:nil];
+    [session setMode:AVAudioSessionModeVoiceChat error:nil]; // تفعيل فلاتر تنقية الصوت
     [session setActive:YES error:nil];
 
-    AVAudioInputNode *input = self.audioEngine.inputNode;
     [self.audioEngine attachNode:self.mixerNode];
     
-    [self.audioEngine connect:input to:self.mixerNode format:[input inputFormatForBus:0]];
-    [self.audioEngine connect:self.mixerNode to:self.audioEngine.mainMixerNode format:[input inputFormatForBus:0]];
+    AVAudioInputNode *input = self.audioEngine.inputNode;
+    AVAudioFormat *format = [input inputFormatForBus:0];
+
+    [self.audioEngine connect:input to:self.mixerNode format:format];
+    [self.audioEngine connect:self.mixerNode to:self.audioEngine.mainMixerNode format:format];
 
     self.mixerNode.outputVolume = self.gainSlider.value;
     [self.audioEngine startAndReturnError:nil];
     
-    [self.powerButton setTitle:@"ON" forState:UIControlStateNormal];
     self.powerButton.backgroundColor = [UIColor systemBlueColor];
-    [self.powerButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [self.powerButton setTitle:@"إيقاف" forState:UIControlStateNormal];
 }
 
 - (void)stopBoosting {
     [self.audioEngine stop];
-    [self.powerButton setTitle:@"OFF" forState:UIControlStateNormal];
     self.powerButton.backgroundColor = [UIColor clearColor];
-    [self.powerButton setTitleColor:[UIColor systemBlueColor] forState:UIControlStateNormal];
+    [self.powerButton setTitle:@"تشغيل" forState:UIControlStateNormal];
 }
 @end
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
-@property (strong, nonatomic) UIWindow *window;
-@end
-@implementation AppDelegate
-- (BOOL)application:(UIApplication *)app didFinishLaunchingWithOptions:(NSDictionary *)opt {
-    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-    self.window.rootViewController = [[RootViewController alloc] init];
-    [self.window makeKeyAndVisible];
-    return YES;
-}
-@end
-
-int main(int argc, char * argv[]) {
-    @autoreleasepool { return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class])); }
-}
+// كود AppDelegate و main يبقى كما هو
