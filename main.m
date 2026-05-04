f4269fbe25900771ef0e45a4f58ab39191aa5ed6
husszzzz <hussien2010rtx@gmail.com>
2026-05-04 11:31:03 +0300

    Implement dark theme and enhance audio UI
    Updated the UI to include a dark theme and added a card view with a power button and gain slider. Enhanced audio session management and improved visual feedback for audio processing.

--- main.m
+++ main.m
@@ -5,63 +5,146 @@
 @property (nonatomic, strong) AVAudioEngine *audioEngine;
 @property (nonatomic, strong) UISlider *gainSlider;
 @property (nonatomic, strong) UILabel *statusLabel;
+@property (nonatomic, strong) UIButton *powerButton;
+@property (nonatomic, strong) UIView *cardView;
 @end
 
 @implementation RootViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor systemGroupedBackgroundColor];
-    self.title = @"Moon Hear Boost";
-    [self setupUI];
+    
+    // 1. الثيم المظلم الاحترافي (Dark Theme)
+    self.title = @"Moon Boost 🎧";
+    self.view.backgroundColor = [UIColor colorWithWhite:0.08 alpha:1.0]; 
+    [self.navigationController.navigationBar setBarStyle:UIBarStyleBlack];
+    [self.navigationController.navigationBar setTitleTextAttributes:@{NSForegroundColorAttributeName: [UIColor whiteColor]}];
+
+    [self setupProUI];
     self.audioEngine = [[AVAudioEngine alloc] init];
 }
 
-- (void)setupUI {
-    self.statusLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 150, self.view.frame.size.width - 40, 30)];
-    self.statusLabel.text = @"جاهز للتشغيل";
+- (void)setupProUI {
+    CGFloat screenWidth = self.view.frame.size.width;
+    
+    // تصميم البطاقة المركزية (Card)
+    self.cardView = [[UIView alloc] initWithFrame:CGRectMake(20, 150, screenWidth - 40, 380)];
+    self.cardView.backgroundColor = [UIColor colorWithWhite:0.15 alpha:1.0];
+    self.cardView.layer.cornerRadius = 30;
+    self.cardView.layer.shadowColor = [UIColor blackColor].CGColor;
+    self.cardView.layer.shadowOpacity = 0.6;
+    self.cardView.layer.shadowRadius = 15;
+    [self.view addSubview:self.cardView];
+
+    // عنوان الحالة
+    self.statusLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 30, self.cardView.frame.size.width, 40)];
+    self.statusLabel.text = @"النظام جاهز";
+    self.statusLabel.textColor = [UIColor whiteColor];
+    self.statusLabel.font = [UIFont systemFontOfSize:24 weight:UIFontWeightBold];
     self.statusLabel.textAlignment = NSTextAlignmentCenter;
-    [self.view addSubview:self.statusLabel];
+    [self.cardView addSubview:self.statusLabel];
 
-    self.gainSlider = [[UISlider alloc] initWithFrame:CGRectMake(50, 250, self.view.frame.size.width - 100, 50)];
-    self.gainSlider.minimumValue = 0.0;
-    self.gainSlider.maximumValue = 1.0; 
-    self.gainSlider.value = 0.5;
-    [self.view addSubview:self.gainSlider];
+    // زر التشغيل الدائري (المتوهج)
+    self.powerButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    self.powerButton.frame = CGRectMake((self.cardView.frame.size.width - 130) / 2, 100, 130, 130);
+    self.powerButton.backgroundColor = [UIColor systemBlueColor];
+    self.powerButton.layer.cornerRadius = 65; // يجعله دائرياً
+    [self.powerButton setTitle:@"تشغيل" forState:UIControlStateNormal];
+    self.powerButton.titleLabel.font = [UIFont systemFontOfSize:26 weight:UIFontWeightHeavy];
+    
+    // إضافة تأثير التوهج للزر
+    self.powerButton.layer.shadowColor = [UIColor systemBlueColor].CGColor;
+    self.powerButton.layer.shadowOpacity = 0.8;
+    self.powerButton.layer.shadowRadius = 20;
+    
+    [self.powerButton addTarget:self action:@selector(toggleAudio) forControlEvents:UIControlEventTouchUpInside];
+    [self.cardView addSubview:self.powerButton];
 
-    UIButton *startBtn = [UIButton buttonWithType:UIButtonTypeSystem];
-    startBtn.frame = CGRectMake(100, 350, self.view.frame.size.width - 200, 60);
-    [startBtn setTitle:@"بدء / إيقاف" forState:UIControlStateNormal];
-    startBtn.backgroundColor = [UIColor systemBlueColor];
-    [startBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    startBtn.layer.cornerRadius = 15;
-    [startBtn addTarget:self action:@selector(toggleAudio) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:startBtn];
+    // نص السلايدر
+    UILabel *sliderTitle = [[UILabel alloc] initWithFrame:CGRectMake(0, 260, self.cardView.frame.size.width, 30)];
+    sliderTitle.text = @"قوة التضخيم (Gain)";
+    sliderTitle.textColor = [UIColor lightGrayColor];
+    sliderTitle.font = [UIFont systemFontOfSize:16 weight:UIFontWeightMedium];
+    sliderTitle.textAlignment = NSTextAlignmentCenter;
+    [self.cardView addSubview:sliderTitle];
+
+    // سلايدر التضخيم (من 1 إلى 15 ضعف)
+    self.gainSlider = [[UISlider alloc] initWithFrame:CGRectMake(30, 300, self.cardView.frame.size.width - 60, 40)];
+    self.gainSlider.minimumValue = 1.0;
+    self.gainSlider.maximumValue = 15.0; // قوة مضاعفة 15 مرة!
+    self.gainSlider.value = 3.0;
+    self.gainSlider.minimumTrackTintColor = [UIColor systemOrangeColor];
+    self.gainSlider.maximumTrackTintColor = [UIColor darkGrayColor];
+    
+    // ربط السلايدر لتغيير الصوت باللحظة (بدون إيقاف)
+    [self.gainSlider addTarget:self action:@selector(sliderValueChanged) forControlEvents:UIControlEventValueChanged];
+    [self.cardView addSubview:self.gainSlider];
+}
+
+// دالة تحديث الصوت باللحظة
+- (void)sliderValueChanged {
+    if (self.audioEngine.isRunning) {
+        self.audioEngine.mainMixerNode.outputVolume = self.gainSlider.value;
+    }
 }
 
 - (void)toggleAudio {
     if (self.audioEngine.isRunning) {
         [self.audioEngine stop];
-        self.statusLabel.text = @"توقف التضخيم";
+        
+        // تغيير شكل الزر لحالة الإيقاف
+        self.statusLabel.text = @"تم الإيقاف";
+        self.statusLabel.textColor = [UIColor whiteColor];
+        [self.powerButton setTitle:@"تشغيل" forState:UIControlStateNormal];
+        self.powerButton.backgroundColor = [UIColor systemBlueColor];
+        self.powerButton.layer.shadowColor = [UIColor systemBlueColor].CGColor;
+        
     } else {
         [self startBoosting];
     }
 }
 
 - (void)startBoosting {
+    // 1. تفعيل جلسة الصوت الاحترافية (تسمح بالبلوتوث والسماعات السلكية)
+    AVAudioSession *session = [AVAudioSession sharedInstance];
+    [session setCategory:AVAudioSessionCategoryPlayAndRecord 
+             withOptions:AVAudioSessionCategoryOptionAllowBluetooth | AVAudioSessionCategoryOptionDefaultToSpeaker 
+                   error:nil];
+    [session setActive:YES error:nil];
+
     AVAudioInputNode *input = self.audioEngine.inputNode;
-    self.audioEngine.mainMixerNode.outputVolume = self.gainSlider.value;
-    [self.audioEngine connect:input to:self.audioEngine.mainMixerNode format:[input inputFormatForBus:0]];
-    
+    AVAudioMixerNode *mixer = self.audioEngine.mainMixerNode;
+
+    // 2. ضبط مستوى التضخيم المبدئي
+    mixer.outputVolume = self.gainSlider.value;
+
+    // 3. التوصيل
+    [self.audioEngine connect:input to:mixer format:[input inputFormatForBus:0]];
+
+    // 4. التشغيل
     NSError *error;
     [self.audioEngine startAndReturnError:&error];
-    self.statusLabel.text = error ? @"خطأ" : @"جاري التضخيم...";
+    
+    if (error) {
+        self.statusLabel.text = @"خطأ بالتشغيل!";
+        self.statusLabel.textColor = [UIColor systemRedColor];
+    } else {
+        // تغيير شكل الزر لحالة التضخيم (أحمر خطر 🔥)
+        self.statusLabel.text = @"جاري التضخيم ⚡️";
+        self.statusLabel.textColor = [UIColor systemOrangeColor];
+        [self.powerButton setTitle:@"إيقاف" forState:UIControlStateNormal];
+        self.powerButton.backgroundColor = [UIColor systemRedColor];
+        self.powerButton.layer.shadowColor = [UIColor systemRedColor].CGColor;
+    }
 }
+
 @end
 
+// --- إعدادات بدء التطبيق ---
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 @property (strong, nonatomic) UIWindow *window;
 @end
+
 @implementation AppDelegate
 - (BOOL)application:(UIApplication *)app didFinishLaunchingWithOptions:(NSDictionary *)opt {
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
