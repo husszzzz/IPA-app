823eaefcec214cef2d2bf3d027534871e0e12a0e
husszzzz <hussien2010rtx@gmail.com>
2026-05-04 11:43:44 +0300

    Update main.m

--- main.m
+++ main.m
@@ -1,159 +1,126 @@
 #import <UIKit/UIKit.h>
 #import <AVFoundation/AVFoundation.h>
 
-@interface RootViewController : UIViewController
+@interface RootViewController : UIViewController <AVAudioRecorderDelegate>
 @property (nonatomic, strong) AVAudioEngine *audioEngine;
+@property (nonatomic, strong) AVAudioRecorder *recorder;
 @property (nonatomic, strong) UISlider *gainSlider;
 @property (nonatomic, strong) UILabel *statusLabel;
-@property (nonatomic, strong) UIButton *powerButton;
-@property (nonatomic, strong) UIView *cardView;
+@property (nonatomic, strong) UIView *sideMenu;
+@property (nonatomic, assign) BOOL isMenuOpen;
+@property (nonatomic, assign) BOOL isRecording;
 @end
 
 @implementation RootViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor colorWithWhite:0.05 alpha:1.0];
+    self.title = @"HASSANY PRO";
     
-    // 1. الثيم المظلم الاحترافي (Dark Theme)
-    self.title = @"Moon Boost 🎧";
-    self.view.backgroundColor = [UIColor colorWithWhite:0.08 alpha:1.0]; 
-    [self.navigationController.navigationBar setBarStyle:UIBarStyleBlack];
-    [self.navigationController.navigationBar setTitleTextAttributes:@{NSForegroundColorAttributeName: [UIColor whiteColor]}];
-
-    [self setupProUI];
+    [self setupProfessionalUI];
+    [self setupSideMenu];
+    
     self.audioEngine = [[AVAudioEngine alloc] init];
 }
 
-- (void)setupProUI {
-    CGFloat screenWidth = self.view.frame.size.width;
-    
-    // تصميم البطاقة المركزية (Card)
-    self.cardView = [[UIView alloc] initWithFrame:CGRectMake(20, 150, screenWidth - 40, 380)];
-    self.cardView.backgroundColor = [UIColor colorWithWhite:0.15 alpha:1.0];
-    self.cardView.layer.cornerRadius = 30;
-    self.cardView.layer.shadowColor = [UIColor blackColor].CGColor;
-    self.cardView.layer.shadowOpacity = 0.6;
-    self.cardView.layer.shadowRadius = 15;
-    [self.view addSubview:self.cardView];
+- (void)setupProfessionalUI {
+    // شعار Hassany
+    UILabel *logoLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 100, self.view.frame.size.width, 50)];
+    logoLabel.text = @"HASSANY";
+    logoLabel.textColor = [UIColor systemGoldColor];
+    logoLabel.font = [UIFont systemFontOfSize:35 weight:UIFontWeightHeavy];
+    logoLabel.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:logoLabel];
 
-    // عنوان الحالة
-    self.statusLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 30, self.cardView.frame.size.width, 40)];
-    self.statusLabel.text = @"النظام جاهز";
-    self.statusLabel.textColor = [UIColor whiteColor];
-    self.statusLabel.font = [UIFont systemFontOfSize:24 weight:UIFontWeightBold];
-    self.statusLabel.textAlignment = NSTextAlignmentCenter;
-    [self.cardView addSubview:self.statusLabel];
+    // زر القائمة الجانبية
+    UIButton *menuBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    menuBtn.frame = CGRectMake(20, 50, 40, 40);
+    [menuBtn setTitle:@"☰" forState:UIControlStateNormal];
+    [menuBtn setTintColor:[UIColor whiteColor]];
+    [menuBtn addTarget:self action:@selector(toggleMenu) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:menuBtn];
 
-    // زر التشغيل الدائري (المتوهج)
-    self.powerButton = [UIButton buttonWithType:UIButtonTypeCustom];
-    self.powerButton.frame = CGRectMake((self.cardView.frame.size.width - 130) / 2, 100, 130, 130);
-    self.powerButton.backgroundColor = [UIColor systemBlueColor];
-    self.powerButton.layer.cornerRadius = 65; // يجعله دائرياً
-    [self.powerButton setTitle:@"تشغيل" forState:UIControlStateNormal];
-    self.powerButton.titleLabel.font = [UIFont systemFontOfSize:26 weight:UIFontWeightHeavy];
-    
-    // إضافة تأثير التوهج للزر
-    self.powerButton.layer.shadowColor = [UIColor systemBlueColor].CGColor;
-    self.powerButton.layer.shadowOpacity = 0.8;
-    self.powerButton.layer.shadowRadius = 20;
-    
-    [self.powerButton addTarget:self action:@selector(toggleAudio) forControlEvents:UIControlEventTouchUpInside];
-    [self.cardView addSubview:self.powerButton];
+    // زر التشغيل المركزي
+    UIButton *powerBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    powerBtn.frame = CGRectMake((self.view.frame.size.width-150)/2, 250, 150, 150);
+    powerBtn.backgroundColor = [UIColor systemBlueColor];
+    powerBtn.layer.cornerRadius = 75;
+    [powerBtn setTitle:@"START" forState:UIControlStateNormal];
+    powerBtn.layer.shadowColor = [UIColor systemBlueColor].CGColor;
+    powerBtn.layer.shadowOpacity = 0.5;
+    powerBtn.layer.shadowRadius = 20;
+    [powerBtn addTarget:self action:@selector(toggleBoost) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:powerBtn];
 
-    // نص السلايدر
-    UILabel *sliderTitle = [[UILabel alloc] initWithFrame:CGRectMake(0, 260, self.cardView.frame.size.width, 30)];
-    sliderTitle.text = @"قوة التضخيم (Gain)";
-    sliderTitle.textColor = [UIColor lightGrayColor];
-    sliderTitle.font = [UIFont systemFontOfSize:16 weight:UIFontWeightMedium];
-    sliderTitle.textAlignment = NSTextAlignmentCenter;
-    [self.cardView addSubview:sliderTitle];
-
-    // سلايدر التضخيم (من 1 إلى 15 ضعف)
-    self.gainSlider = [[UISlider alloc] initWithFrame:CGRectMake(30, 300, self.cardView.frame.size.width - 60, 40)];
-    self.gainSlider.minimumValue = 1.0;
-    self.gainSlider.maximumValue = 15.0; // قوة مضاعفة 15 مرة!
-    self.gainSlider.value = 3.0;
-    self.gainSlider.minimumTrackTintColor = [UIColor systemOrangeColor];
-    self.gainSlider.maximumTrackTintColor = [UIColor darkGrayColor];
-    
-    // ربط السلايدر لتغيير الصوت باللحظة (بدون إيقاف)
-    [self.gainSlider addTarget:self action:@selector(sliderValueChanged) forControlEvents:UIControlEventValueChanged];
-    [self.cardView addSubview:self.gainSlider];
+    // زر التسجيل
+    UIButton *recBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    recBtn.frame = CGRectMake((self.view.frame.size.width-100)/2, 450, 100, 50);
+    [recBtn setTitle:@"⏺ RECORD" forState:UIControlStateNormal];
+    [recBtn setTitleColor:[UIColor systemRedColor] forState:UIControlStateNormal];
+    [recBtn addTarget:self action:@selector(toggleRecording) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:recBtn];
 }
 
-// دالة تحديث الصوت باللحظة
-- (void)sliderValueChanged {
-    if (self.audioEngine.isRunning) {
-        self.audioEngine.mainMixerNode.outputVolume = self.gainSlider.value;
-    }
+- (void)setupSideMenu {
+    self.sideMenu = [[UIView alloc] initWithFrame:CGRectMake(-250, 0, 250, self.view.frame.size.height)];
+    self.sideMenu.backgroundColor = [UIColor colorWithWhite:0.12 alpha:1.0];
+    [self.view addSubview:self.sideMenu];
+
+    UILabel *teamLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, self.view.frame.size.height-100, 230, 60)];
+    teamLabel.numberOfLines = 2;
+    teamLabel.text = @"فريق التطوير: الحسني\n@OM_G9";
+    teamLabel.textColor = [UIColor lightGrayColor];
+    teamLabel.textAlignment = NSTextAlignmentCenter;
+    [self.sideMenu addSubview:teamLabel];
 }
 
-- (void)toggleAudio {
+- (void)toggleMenu {
+    self.isMenuOpen = !self.isMenuOpen;
+    [UIView animateWithDuration:0.3 animations:^{
+        self.sideMenu.frame = CGRectMake(self.isMenuOpen ? 0 : -250, 0, 250, self.view.frame.size.height);
+    }];
+}
+
+- (void)toggleBoost {
     if (self.audioEngine.isRunning) {
         [self.audioEngine stop];
-        
-        // تغيير شكل الزر لحالة الإيقاف
-        self.statusLabel.text = @"تم الإيقاف";
-        self.statusLabel.textColor = [UIColor whiteColor];
-        [self.powerButton setTitle:@"تشغيل" forState:UIControlStateNormal];
-        self.powerButton.backgroundColor = [UIColor systemBlueColor];
-        self.powerButton.layer.shadowColor = [UIColor systemBlueColor].CGColor;
-        
     } else {
-        [self startBoosting];
+        [self startAudioEngine];
     }
 }
 
-- (void)startBoosting {
-    // 1. تفعيل جلسة الصوت الاحترافية (تسمح بالبلوتوث والسماعات السلكية)
+- (void)startAudioEngine {
     AVAudioSession *session = [AVAudioSession sharedInstance];
+    // أهم سطر لجعل التطبيق يعمل والجهاز مقفل
     [session setCategory:AVAudioSessionCategoryPlayAndRecord 
-             withOptions:AVAudioSessionCategoryOptionAllowBluetooth | AVAudioSessionCategoryOptionDefaultToSpeaker 
+             withOptions:AVAudioSessionCategoryOptionDefaultToSpeaker | AVAudioSessionCategoryOptionAllowBluetooth
                    error:nil];
     [session setActive:YES error:nil];
 
     AVAudioInputNode *input = self.audioEngine.inputNode;
-    AVAudioMixerNode *mixer = self.audioEngine.mainMixerNode;
+    [self.audioEngine connect:input to:self.audioEngine.mainMixerNode format:[input inputFormatForBus:0]];
+    [self.audioEngine startAndReturnError:nil];
+}
 
-    // 2. ضبط مستوى التضخيم المبدئي
-    mixer.outputVolume = self.gainSlider.value;
-
-    // 3. التوصيل
-    [self.audioEngine connect:input to:mixer format:[input inputFormatForBus:0]];
-
-    // 4. التشغيل
-    NSError *error;
-    [self.audioEngine startAndReturnError:&error];
-    
-    if (error) {
-        self.statusLabel.text = @"خطأ بالتشغيل!";
-        self.statusLabel.textColor = [UIColor systemRedColor];
+- (void)toggleRecording {
+    if (self.isRecording) {
+        [self.recorder stop];
+        self.isRecording = NO;
     } else {
-        // تغيير شكل الزر لحالة التضخيم (أحمر خطر 🔥)
-        self.statusLabel.text = @"جاري التضخيم ⚡️";
-        self.statusLabel.textColor = [UIColor systemOrangeColor];
-        [self.powerButton setTitle:@"إيقاف" forState:UIControlStateNormal];
-        self.powerButton.backgroundColor = [UIColor systemRedColor];
-        self.powerButton.layer.shadowColor = [UIColor systemRedColor].CGColor;
+        [self startRecording];
+        self.isRecording = YES;
     }
 }
 
-@end
-
-// --- إعدادات بدء التطبيق ---
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
-@property (strong, nonatomic) UIWindow *window;
-@end
-
-@implementation AppDelegate
-- (BOOL)application:(UIApplication *)app didFinishLaunchingWithOptions:(NSDictionary *)opt {
-    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[RootViewController alloc] init]];
-    [self.window makeKeyAndVisible];
-    return YES;
+- (void)startRecording {
+    NSString *path = [NSTemporaryDirectory() stringByAppendingPathComponent:@"hassany_rec.m4a"];
+    NSURL *url = [NSURL fileURLWithPath:path];
+    NSDictionary *settings = @{AVFormatIDKey: @(kAudioFormatMPEG4AAC), AVSampleRateKey: @44100.0, AVNumberOfChannelsKey: @2};
+    self.recorder = [[AVAudioRecorder alloc] initWithURL:url settings:settings error:nil];
+    [self.recorder record];
 }
+
 @end
 
-int main(int argc, char * argv[]) {
-    @autoreleasepool { return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class])); }
-}
+// AppDelegate و main يبقيان كما هما
