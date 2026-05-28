c3231529421dcf4f2cc9b1a28a0bcf0cc0b55f85
husszzzz <hussien2010rtx@gmail.com>
2026-05-28 20:24:21 +0300

    Add comments in Arabic and improve ghost behavior

--- RootViewController.m
+++ RootViewController.m
@@ -5,18 +5,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor blackColor];
-    self.currentState = 1; // 1 يعني Playing
+    self.currentState = 1; // 1 يعني حالة اللعب (Playing)
     
     // إعداد اللاعب
     self.playerImageView = [[UIImageView alloc] initWithFrame:CGRectMake(self.view.center.x - 30, self.view.center.y - 30, 60, 60)];
     self.playerImageView.image = [UIImage imageNamed:@"player_back.png"];
     [self.view addSubview:self.playerImageView];
     
-    // إعداد الوحش (ghostImageView)
+    // إعداد الوحش (تأكدنا إن اسمه ghostImageView مثل ملف الـ h)
     self.ghostImageView = [[UIImageView alloc] initWithFrame:CGRectMake(50, 50, 70, 70)];
     self.ghostImageView.image = [UIImage imageNamed:@"ghost.png"];
     [self.view addSubview:self.ghostImageView];
     
+    // استدعاء دالة بناء الجويستيك
+    [self createGameControls];
+    
+    // تشغيل العداد (Game Loop)
     self.gameTimer = [CADisplayLink displayLinkWithTarget:self selector:@selector(updateGameLoop:)];
     [self.gameTimer addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
 }
@@ -24,7 +28,7 @@
 - (void)updateGameLoop:(CADisplayLink *)sender {
     if (self.currentState != 1) return;
 
-    // حركة بسيطة للاعب
+    // حركة اللاعب وتدويره
     if (self.moveVelocity.x != 0 || self.moveVelocity.y != 0) {
         CGFloat angle = atan2(self.moveVelocity.y, self.moveVelocity.x);
         self.playerImageView.transform = CGAffineTransformMakeRotation(angle);
@@ -33,8 +37,28 @@
     // حركة الوحش (تطارد اللاعب)
     CGFloat dx = self.playerImageView.center.x - self.ghostImageView.center.x;
     CGFloat dy = self.playerImageView.center.y - self.ghostImageView.center.y;
-    self.ghostImageView.center = CGPointMake(self.ghostImageView.center.x + (dx * 0.02), 
-                                             self.ghostImageView.center.y + (dy * 0.02));
+    CGFloat distance = sqrt(dx*dx + dy*dy);
+    
+    if (distance > 50) {
+        self.ghostImageView.center = CGPointMake(self.ghostImageView.center.x + (dx * 0.02), 
+                                                 self.ghostImageView.center.y + (dy * 0.02));
+    } else {
+        [self triggerJumpscareAttack];
+    }
+}
+
+// ----------------------------------------------------
+// الدوال الناقصة اللي سببت الخطأ (ضفناها حتى يكتمل البناء)
+// ----------------------------------------------------
+
+- (void)createGameControls {
+    // هنا مكان كود أزرار التحكم (الجويستيك)
+    // تركناها مبسطة حالياً حتى نتجاوز خطأ البناء
+}
+
+- (void)triggerJumpscareAttack {
+    // هنا كود الرعب (Jumpscare) لما يلمسك الوحش
+    self.currentState = 0; // إيقاف اللعبة عند الخسارة
 }
 
 @end
