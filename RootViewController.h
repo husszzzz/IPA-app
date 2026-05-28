87fbb59e5fab6e4bba29d880ef4a42c418f3f6a4
husszzzz <hussien2010rtx@gmail.com>
2026-05-28 20:20:55 +0300

    Update RootViewController.h

--- RootViewController.h
+++ RootViewController.h
@@ -1,36 +1,32 @@
 #import <UIKit/UIKit.h>
 #import <AVFoundation/AVFoundation.h>
 
-// تعريف حالات اللعبة
-typedef NS_ENUM(NSInteger, GameState) {
-    GameStateMenu,
-    GameStatePlaying,
-    GameStateJumpscare
-};
-
 @interface RootViewController : UIViewController
 
-// واجهات القوائم والأزرار
-@property (nonatomic, strong) UIView *menuView;          // قائمة البداية
-@property (nonatomic, strong) UILabel *gameTitleLabel;   // عنوان اللعبة المشع
-@property (nonatomic, strong) UIButton *startButton;     // زر ابدأ اللعبة الفخم
-@property (nonatomic, strong) UIView *joystickBase;      // قاعدة الجويستيك
-@property (nonatomic, strong) UIView *joystickKnob;      // عصا التحكم
-@property (nonatomic, strong) UIButton *actionButton;    // زر تفاعلي مطور (فلاش/ركض)
-
-// كائنات اللعبة والميديا
-@property (nonatomic, strong) UIImageView *roomBackground;
+// عناصر اللعبة الأساسية
+@property (nonatomic, strong) UIImageView *playerImageView;
 @property (nonatomic, strong) UIImageView *ghostImageView;
-@property (nonatomic, strong) UIView *playerNode;        // كائن اللاعب المضيء
+@property (nonatomic, strong) UIImageView *roomBackground;
+@property (nonatomic, strong) UIView *joystickBase;
+@property (nonatomic, strong) UIView *joystickKnob;
+
+// متغيرات الحركة والتحكم
+@property (nonatomic, assign) CGPoint playerPos;
+@property (nonatomic, assign) CGPoint moveVelocity;
+@property (nonatomic, assign) BOOL isJoystickTouching;
+@property (nonatomic, assign) CGPoint joystickCenter;
+
+// نظام اللعبة والوقت
+@property (nonatomic, assign) NSInteger currentState;
+@property (nonatomic, strong) CADisplayLink *gameTimer;
+
+// العناصر الصوتية
 @property (nonatomic, strong) AVAudioPlayer *jumpscareSound;
 @property (nonatomic, strong) AVAudioPlayer *bgmSound;
 
-// منطق الـ Engine والحركة
-@property (nonatomic, strong) CADisplayLink *gameTimer;  // حلقة اللعبة فائقة السلاسة
-@property (nonatomic, assign) GameState currentState;
-@property (nonatomic, assign) CGPoint playerPos;
-@property (nonatomic, assign) CGPoint moveVelocity;
-@property (nonatomic, assign) CGPoint joystickCenter;
-@property (nonatomic, assign) BOOL isJoystickTouching;
+// دوال التحكم
+- (void)createGameControls;
+- (void)updateGameLoop:(CADisplayLink *)sender;
+- (void)triggerJumpscareAttack;
 
 @end
